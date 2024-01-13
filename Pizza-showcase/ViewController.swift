0b7228d7e6dbb5e92e82105fba24b6ccf2cd21f0
Ruslan <hellperock@gmail.com>
2024-01-13 10:28:54 +0500

    Initial Commit

---
+++ Pizza-showcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizza-showcase
+//
+//  Created by Ruslan Ismailov on 13/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
